f768f5cbff42437580851ef1246179c796159ec4
juliengoff <Administrateur@147-UC47-07.ad.campus-eni.fr>
2018-02-21 15:14:29 +0100

    ApiRoute added

--- Parkitect/Controllers/ApiParkingController.cs
+++ Parkitect/Controllers/ApiParkingController.cs
@@ -2,12 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 using Tools;
 
---
+++ bo/Tarif.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bo
+{
+    class Tarif
+    {
+        public double price { get; set; }
+
+        public DateTime horaire { get; set; }
+
+        public int duration { get; set; } // event duration in minutes
+
+        public Tarif()
+        {
+            
+        }
+    }
+}
