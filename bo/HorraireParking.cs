8f399d4486618b98831c45bd0eb2766bc97c6a93
nicolasdchartier <nicolas.chartier2016@campus-eni.fr>
2018-02-26 16:17:55 +0100

    gestion des horaires

---
+++ bo/HorraireParking.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace bo
+{
+    public class HoraireParking
+    {
+        public string Id { get; set; }
+
+        public string Parking { get; set; }
+
+        public int Jours { get; set; }
+
+        public DateTime CloseTime { get; set; }
+
+        public DateTime OpenTime { get; set; }
+        public Parking()
+        {
+
+        }
+
+        public Boolean isParkingOpen(DateTime currentTime, DateTime CloseTime, DateTime OpenTime)
+        {
+            bool isOpen = true;
+
+            if (currentTime > CloseTime && currentTime < OpenTime)
+            {
+                isOpen = false;
+            }
+
+            return isOpen;
+        }
+    }
+}
---
+++ bo/TypeJours.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace bo
+{
+
+        public enum Jour
+        {
+            Dimanche = 0,
+            Lundi = 1,
+            Mardi = 2,
+            Mercredi = 3,
+            Jeudi = 4,
+            Vendredi = 5,
+            Samedi = 6,
+            Permanent = 7
+        }
+
+        public static class EnumExtension
+        {
+            public static int GetValue(this Jour nb)
+            {
+                return (int)nb;
+            }
+        }
+    }
+}
