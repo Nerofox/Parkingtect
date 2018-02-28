9b09021152830e195ad20749025e1a53430618c3
Quentin <pismont.quentin@orange.fr>
2018-02-28 16:08:25 +0100

    trajet parking evenement

--- Parkitect/Controllers/ApiMapController.cs
+++ Parkitect/Controllers/ApiMapController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using Tools;
 using bll;
+using bo;
 
 namespace Parkitect.Controllers
 {
@@ -28,6 +29,50 @@ namespace Parkitect.Controllers
             return JsonConvert.SerializeObject(endPointString);
         }
 
+        public async Task<string> GetParkingsEvenement(int id)
+        {
+            var evenementService = new EvenementService();
+            var evenement = evenementService.Find(id);
+            var str = (API_MAPBOX + "geocoding/v5/mapbox.places/" + evenement.Address + " " + evenement.City + ".json?access_token=" + API_TOKEN).Replace(" ", "%20");
+            dynamic evenementLocation = Api.Get(str).features[0].geometry.coordinates;
+
+            dynamic data = Api.Get(API_PARKING + "parks/?crs=EPSG:4326");
+            var lon = Request.QueryString["lon"].Replace(",", ".");
+            var lat = Request.QueryString["lat"].Replace(",", ".");
+
+            List<Trajet> trajets = new List<Trajet>();
+            for (int i = 0; i < data.parks.Count; i++)
+            {
+                for (int j = 0; j < data.features.features.Count; j++)
+                {
+                    dynamic oneElement = data.features.features[j];
+                    if (oneElement.id.ToString() == data.parks[i].id.ToString())
+                    {
+                        var parkingLocation = oneElement.geometry.coordinates;
+                        dynamic parkingInfo = new { Parking = data.parks[i], Location = parkingLocation };
+
+                        trajets.Add(new Trajet(parkingInfo));
+
+                        string urlDistance = API_MAPBOX + "directions/v5/mapbox/driving/" + lon + "," + lat + ";" + parkingLocation[0].ToString().Replace(",", ".") + "," + parkingLocation[1].ToString().Replace(",", ".") + "?access_token=" + API_TOKEN + "&geometries=geojson";
+                        dynamic userToParking = Api.Get(urlDistance);
+                        trajets[trajets.Count - 1].Duration += Convert.ToDouble(userToParking.routes[0].duration);
+                        trajets[trajets.Count - 1].Distance += Convert.ToDouble(userToParking.routes[0].distance);
+                        trajets[trajets.Count - 1].DrivingDirection = userToParking.routes[0].geometry;
+
+                        urlDistance = API_MAPBOX + "directions/v5/mapbox/walking/" + evenementLocation[0].ToString().Replace(",", ".") + "," + evenementLocation[1].ToString().Replace(",", ".") + ";" + parkingLocation[0].ToString().Replace(",", ".") + "," + parkingLocation[1].ToString().Replace(",", ".") + "?access_token=" + API_TOKEN + "&geometries=geojson";
+                        dynamic evtToParking = Api.Get(urlDistance);
+                        trajets[trajets.Count - 1].Duration += Convert.ToDouble(evtToParking.routes[0].duration);
+                        trajets[trajets.Count - 1].Distance += Convert.ToDouble(evtToParking.routes[0].distance);
+                        trajets[trajets.Count - 1].WalkingDirection = evtToParking.routes[0].geometry;
+                        break;
+                    }
+                }
+            }
+
+            var sortList = trajets.OrderBy(t => t.Distance).Take(3).Reverse();
+            return JsonConvert.SerializeObject(sortList);
+        }
+
         // GET: ApiMap
         public async Task<string> BestItiParking()
         {
@@ -35,8 +80,8 @@ namespace Parkitect.Controllers
             ApiParkingController apiPark = new ApiParkingController();
             dynamic data = Api.Get(API_PARKING + "parks/?crs=EPSG:4326");
 
-            var lon = Request.QueryString["lon"];
-            var lat = Request.QueryString["lat"];
+            var lon = Request.QueryString["lon"].Replace(",", ".");
+            var lat = Request.QueryString["lat"].Replace(",", ".");
 
             for (int i = 0; i < data.parks.Count; i++)
             {
---
+++ bo/Trajet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bo
+{
+    public class Trajet
+    {
+
+        public dynamic Parking { get; set; }
+
+        public dynamic DrivingDirection { get; set; }
+
+        public dynamic WalkingDirection { get; set; }
+
+        public int Duration { get; set; }
+
+        public int Distance { get; set; }
+
+
+        public Trajet(dynamic parking)
+        {
+            this.Parking = parking;
+        }
+    }
+}
